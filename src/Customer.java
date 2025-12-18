22126f8980d5dd727c309ccc3306c5a9733fe11d
bjw446 <bjw446@naver.com>
2025-12-18 20:48:57 +0900

    상품 카테고리와 고객관리 클래스

---
+++ src/Customer.java
@@ -0,0 +1,24 @@
+public class Customer {
+
+    private String customerName;
+    private String customerEmail;
+    private String customerGrade;
+
+    public Customer() {
+    }
+
+
+    public String getCustomerName() {
+        return customerName;
+    }
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+    public String getCustomerGrade() {
+        return customerGrade;
+    }
+
+}
+
+
+
